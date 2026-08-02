93e0e5461808712d671b352eb2c03d8bb10540a9
Drue Jaramillo <djaramillo3@outlook.com>
2026-08-02 13:01:54 -0600

    update vite config for remote server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    allowedHosts: true,
     proxy: {
       "/api": "http://127.0.0.1:8787",
       "/media": "http://127.0.0.1:8787",
